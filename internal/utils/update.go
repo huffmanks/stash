4d3fee79002639f4bb9210b4cd61c3fb001c8826
Kevin Huffman <huffmanks2@gmail.com>
2026-09-20 11:59:14 -0400

    update.go to use releases binary

--- internal/utils/update.go
+++ internal/utils/update.go
@@ -1,10 +1,17 @@
 package utils
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"context"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/huffmanks/stash/internal/config"
@@ -37,22 +44,117 @@ func HandleUpdate(banner string, force bool, latest string) {
 	spinner.Start("Updating...")
 	time.Sleep(time.Millisecond * 100)
 
-	scriptURL := "https://raw.githubusercontent.com/huffmanks/stash/main/install.sh"
-	shellCmd := fmt.Sprintf("curl -sSL %s | bash -s --", scriptURL)
+	versionClean := strings.TrimPrefix(latest, "v")
+	binaryName := fmt.Sprintf("stash_%s_%s_%s", versionClean, runtime.GOOS, runtime.GOARCH)
+	downloadURL := fmt.Sprintf("https://github.com/huffmanks/stash/releases/download/%s/%s.tar.gz", latest, binaryName)
 
-	if force {
-		shellCmd += " --force"
+	tmpDir, err := os.MkdirTemp("", "stash-update-*")
+	if err != nil {
+		spinner.Stop("❌ [FAILED]: creating temporary directory.", 2)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	resp, err := http.Get(downloadURL)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		if resp != nil {
+			resp.Body.Close()
+		}
+		spinner.Stop(fmt.Sprintf("❌ [FAILED]: downloading release from %s", downloadURL), 2)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	gzr, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		spinner.Stop("❌ [FAILED]: reading gzip stream.", 2)
+		os.Exit(1)
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+	var extractedBinaryPath string
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			spinner.Stop("❌ [FAILED]: reading archive.", 2)
+			os.Exit(1)
+		}
+
+		target := filepath.Join(tmpDir, header.Name)
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(target, 0755); err != nil {
+				spinner.Stop("❌ [FAILED]: creating directory structure.", 2)
+				os.Exit(1)
+			}
+		case tar.TypeReg:
+			if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
+				spinner.Stop("❌ [FAILED]: creating parent directory.", 2)
+				os.Exit(1)
+			}
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				spinner.Stop("❌ [FAILED]: extracting file.", 2)
+				os.Exit(1)
+			}
+			if _, err := io.Copy(f, tr); err != nil {
+				f.Close()
+				spinner.Stop("❌ [FAILED]: writing extracted file.", 2)
+				os.Exit(1)
+			}
+			f.Close()
+
+			if filepath.Base(target) == "stash" {
+				extractedBinaryPath = target
+			}
+		}
 	}
 
-	cmd := exec.Command("sh", "-c", shellCmd)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	if extractedBinaryPath == "" {
+		spinner.Stop("❌ [FAILED]: 'stash' binary not found in archive.", 2)
+		os.Exit(1)
+	}
 
+	spinner.Message("Installing binary...")
+
+	execPath, err := os.Executable()
 	if err != nil {
-		spinner.Stop("❌ [FAILED]: updating stash.", 2)
-		time.Sleep(time.Millisecond * 100)
+		installDir := "/usr/local/bin"
+		if os.Getuid() != 0 {
+			if home, err := os.UserHomeDir(); err == nil {
+				installDir = filepath.Join(home, ".local", "bin")
+			}
+		}
+		execPath = filepath.Join(installDir, "stash")
+	}
+
+	var cpCmd, chmodCmd *exec.Cmd
+	if strings.HasPrefix(execPath, "/usr/local/bin") && os.Getuid() != 0 {
+		cpCmd = exec.Command("sudo", "cp", extractedBinaryPath, execPath)
+		chmodCmd = exec.Command("sudo", "chmod", "+x", execPath)
+	} else {
+		if err := os.MkdirAll(filepath.Dir(execPath), 0755); err != nil {
+			spinner.Stop("❌ [FAILED]: creating install directory.", 2)
+			os.Exit(1)
+		}
+		cpCmd = exec.Command("cp", extractedBinaryPath, execPath)
+		chmodCmd = exec.Command("chmod", "+x", execPath)
+	}
+
+	cpCmd.Stdout = os.Stdout
+	cpCmd.Stderr = os.Stderr
+	if err := cpCmd.Run(); err != nil {
+		spinner.Stop("❌ [FAILED]: installing binary to destination.", 2)
+		os.Exit(1)
+	}
+
+	if err := chmodCmd.Run(); err != nil {
+		spinner.Stop("❌ [FAILED]: setting executable permissions.", 2)
 		os.Exit(1)
 	}
 
